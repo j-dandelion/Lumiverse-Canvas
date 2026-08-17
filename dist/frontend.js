2c66d5e2e6bb367548d9dc1bc1a87fec34659f64
j-dandelion <j-dandelion@users.noreply.github.com>
2026-08-17 12:42:40 -0600

    chore: bump version to 1.9.0

--- dist/frontend.js
+++ dist/frontend.js
@@ -179,7 +179,7 @@ function getBackendCtx() {
 function setBackendCtx(ctx) {
   _backendCtx = ctx;
 }
-var _backendCtx = null, CANVAS_VERSION = "1.8.0.26";
+var _backendCtx = null, CANVAS_VERSION = "1.9.0";
 
 // src/debug/log.ts
 function setDebug(value) {
